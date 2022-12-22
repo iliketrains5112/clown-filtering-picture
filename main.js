18490fa1d0a607b8ca3c9fffd8ef53901415853b
iliketrains <84407492+iliketrains5112@users.noreply.github.com>
2022-12-22 16:47:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-noseX = 0
-noseY = 0
+noseX = 0;
+noseY = 0;
 
 function preload(){
 clownNose = loadImage('https://i.postimg.cc/x1gvQxM1/clown-Nose.png');
@@ -9,11 +9,25 @@ function setup(){
     canvas=createCanvas(300, 300);
     canvas.center();
     video = createCapture(VIDEO);
+    video.size(300, 300);
     video.hide();
 
     poseNet=ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
 }
+function draw(){
+image(video, 0, 0, 300, 300);
+image(clownNose, noseX-15, noseY-20, 35,35);
+}
+
+function takeSnapshot(){
+    save("clowned_image.png");
+}
+
+function modelLoaded(){
+    console.log("PoseNet is initialized");
+}
+
 function gotPoses(results){
     if (results.length > 0 ){
         console.log(results)
@@ -24,16 +38,3 @@ function gotPoses(results){
         noseY = results[0].pose.nose.y;
     }
     }
-function draw(){
-image(video, 0, 0, 300, 300);
-image(clownNose, noseX, noseY, 25, 25);
-}
-
-function takeSnapshot(){
-    save("clowned_image");
-}
-
-function modelLoaded(){
-    console.log("PoseNet is initialized");
-}
-
